e14094741df823a3d9ee46f8913a66166dbe5419
DaniIliev <danieljivkov04@abv.bg>
2025-12-19 12:35:16 +0200

    delete performance controller

--- src/routes/performance-data.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getPerformanceData } from "../controllers/performanceController.js";
-import authMiddleware from "../middlewares/auth.js";
-
-const router = express.Router();
-
-router.get("/", authMiddleware, getPerformanceData);
-
-export default router;
